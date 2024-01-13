cd4ee8ad6ecd0436f7f21b91870a7882102f3cfa
tarnovskii <artem3dgame@gmail.com>
2024-01-13 05:28:12 +0200

    complete landing ui mobile

--- src/components/landing-components/IntroBanner/IntroBanner.jsx
+++ src/components/landing-components/IntroBanner/IntroBanner.jsx
@@ -6,15 +6,20 @@ export const IntroBanner = () => {
     return (
         <section className='con-def ib'>
             <div className='ib__text'>
-                <h1>Safe investments<br/>up to 200% in BNB<br/><span>on your deposit</span></h1>
+                <h1>Safe investments up to 200% in BNB <span>on your deposit</span></h1>
                 <p>Best offer on the market</p>
-                <div className='ib__text__buttons'>
+                <div className='ib__text__buttons con-desk'>
                     <button className='red-bttn mid-bttn'>Make a deposit</button>
                     <button className='trnp-bttn mid-bttn'>Verified contract</button>
                     <button className='trnp-bttn mid-bttn'>Audit results</button>
                 </div>
             </div>
             <img className='ib__image' src={intoBannerImage} alt={'into-banner-image'}/>
+            <div className='ib__text__buttons con-mob'>
+                <button className='red-bttn mid-bttn'>Make a deposit</button>
+                <button className='trnp-bttn mid-bttn'>Verified contract</button>
+                <button className='trnp-bttn mid-bttn'>Audit results</button>
+            </div>
         </section>
     )
 }
